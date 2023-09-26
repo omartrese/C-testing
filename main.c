4cfdd12244c4d0e053fe4b421c82662f14a4bb15
omartrese <omarlayachi13@gmail.com>
2023-09-26 19:15:50 +0200

    three-coins

--- main.c
+++ main.c
@@ -4,50 +4,66 @@
 
 void convert(int type, float money)
 {
-    switch(type)
+    float euros, dollars, pesos;
+    if(money < 0) return;
+
+    switch (type)
     {
-        float money, euros, dollars, pesos;
-        
         case 0:
-            
+            dollars = money / 0.95;
+            pesos = money * 18.5;
+            printf("%f EUR\nCONVERSIONES:\nEuros a:\nDólares --> %f\nPesos Mexicanos --> %f\n", money, dollars, pesos);
             break;
 
-        case 1: 
-            
+        case 1:
+            euros = money * 0.95;
+            pesos = money * 17.5;
+            printf("%f USD\nCONVERSIONES:\nDólares a:\nEuros --> %f\nPesos Mexicanos --> %f\n", money, euros, pesos);
             break;
 
-        case 2: 
-
+        case 2:
+            euros = money / 18.5;
+            dollars = money / 17.5;
+            printf("%f Pesos Mexicanos\nCONVERSIONES:\nPesos a:\nEuros --> %f\nDólares --> %f\n", money, euros, dollars);
             break;
 
+        default:
+            printf("No es un tipo de moneda válido.\n");
+            break;
     }
+
+
 }
 
 void checkMoneyType(int type)
 {   
+    float money;
+    
     switch(type)
     {
-        float money;
         
         case 0:
             printf("\nCuantos EUROS quieres convertir?\n\n");
             scanf("%f", &money);
+            printf("\n\n");
             convert(type, money);
-            fflush(stdin);
+            
             break;
 
         case 1: 
             printf("\nCuantos DÓLARES quieres convertir?\n\n");
             scanf("%f", &money);
+            printf("\n\n");
             convert(type, money);
-            fflush(stdin);
+            
             break;
 
         case 2: 
             printf("\nCuantos PESOS quieres convertir?\n\n");
             scanf("%f", &money);
+            printf("\n\n");
             convert(type, money);
-            fflush(stdin);
+            
             break;
 
         default:
